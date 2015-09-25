0058bd9041a4de6b83da72373825baec9f07453a
Ryan Densmore <rdensmore365@gmail.com>
2015-09-24 20:49:21 -0400

    save

--- blinky.py
+++ blinky.py
@@ -4,20 +4,17 @@ import RPi.GPIO as GPIO
 import time as t
 
 pins  = [4,27,5,26]
+pins1 = [26,5,27,4] 
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pins,GPIO.OUT)
+GPIO.setup(pins1,GPIO.OUT)
 
 def on(pin):
     GPIO.output(pin,GPIO.HIGH)
 
 def off(pin):
     GPIO.output(pin,GPIO.LOW)
-
-def slowon(list=pins,delay=.1): 
-    for pin in list:
-        on(pin)
-        t.sleep(delay)
 
 def slowoff(list=pins,delay=.1): 
     for pin in list:
@@ -26,7 +23,22 @@ def slowoff(list=pins,delay=.1):
 
 def chase(count=1):
     for n in range(count):
-    slowon()
-    slowoff()
+        slowon()
+        slowoff()
 
+def baf(baf=1):
+    for n in range(baf):
+        for pin in pins:
+	    bky.on(pin)
+    	    t.sleep(.1)
+        for pin in pins:
+    	    bky.off(pin)
+            t.sleep(.1)
+
+        for pin in bky.pins[::-1]:
+            bky.on(pin)
+            t.sleep(.1)
+        for pin in bky.pins[::-1]:
+            bky.off(pin)
+            t.sleep(.1)
 
