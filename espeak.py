b888cc56ce413d15f3d99acf31fd6671a0a02727
Ryan Densmore <rdensmore365@gmail.com>
2015-12-03 20:13:49 -0500

    save

--- espeak.py
+++ espeak.py
@@ -4,3 +4,6 @@ def say(text=None,voice=None):
     text = "'" + text + "'"
     if voice != None:
         voice = " -v " + voice
+    else:
+        voice = ""
+    sp.call("espeak" + voice + text, shell=True)
